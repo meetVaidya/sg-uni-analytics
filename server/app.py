8e30dd9124811c231e4c702972c1597d6b7bd400
meext <vaidyameet2004@gmail.com>
2024-04-27 16:30:03 +0530

    Update model loading and prediction logic in app.py

--- server/app.py
+++ server/app.py
@@ -12,16 +12,24 @@ CORS(app)
 
 # Load your trained machine learning model
 try:
-    model = joblib.load('model.pkl')
-    print(f"Model loaded: {model is not None}")
+    model1 = joblib.load('model1.pkl')
+    print(f"Model loaded: {model1 is not None}")
 except Exception as e:
     print(f"Error loading model: {e}")
-    model = None
+    model1 = None
+try:
+    model2 = joblib.load('model2.pkl')
+    print(f"Model loaded: {model2 is not None}")
+except Exception as e:
+    print(f"Error loading model: {e}")
+    model2 = None
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if model == None:
+    if model1 == None:
+        return jsonify({'error': 'Model is not loaded'})
+    if model1 == None:
         return jsonify({'error': 'Model is not loaded'})
 
     try :
@@ -49,18 +57,26 @@ def predict():
         
         print(input_data)
 
-        prediction = model.predict(input_data)
+        prediction1 = model1.predict(input_data)
+        prediction2 = model2.predict(input_data)
 
-        print(f"Prediction type: {type(prediction)}")
-        print(f"Prediction value: {prediction}")
+        print(f"Prediction type: {type(prediction1)}")
+        print(f"Prediction value: {prediction1}")
+        # print(f"Prediction type: {type(prediction2)}")
+        print(f"Prediction value: {prediction2}")
 
-        if isinstance(prediction, np.ndarray):
-            employment_rate = prediction[0]  # For numpy array
+        if isinstance(prediction1, np.ndarray):
+            employment_rate = prediction1[0]  # For numpy array
         else:
-            employment_rate = prediction.values[0]  # For pandas Series
-
-        response = {'employment_rate': employment_rate}
+            employment_rate = prediction1.values[0]  # For pandas Series
+        if isinstance(prediction2, np.ndarray):
+            yearly_basic_salary = prediction2[0]  # For numpy array
+        else:
+            yearly_basic_salary = prediction2.values[0]  # For pandas Series
+    
+        response = {'employment_rate': employment_rate, 'yearly_basic_salary': yearly_basic_salary}
         print(f"Response: {response}")
+
         return jsonify(response)
     except Exception as e:
         return jsonify({'error': str(e)})
